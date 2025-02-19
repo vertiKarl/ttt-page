4d417b67a338e56c8535c8d1ed2458081483c346
vertiKarl <vertikarl@proton.me>
2025-02-19 20:34:43 +0100

    Changed system to a redirect

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
